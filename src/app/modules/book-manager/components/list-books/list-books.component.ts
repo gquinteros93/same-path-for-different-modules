765bb6629a7707adc2258c1aa7699ec82296e480
German Quinteros <german.quinteros.1993@gmail.com>
2019-10-03 19:47:41 -0300

    Start Plate Field.

--- src/app/modules/book-manager/components/list-books/list-books.component.ts
+++ src/app/modules/book-manager/components/list-books/list-books.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from 'src/app/shared/models/book';
 import { BookService } from 'src/app/shared/services/book.service';
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './list-books.component.html',
   styleUrls: ['./list-books.component.scss']
 })
-export class ListBooksComponent implements OnInit, OnDestroy {
+export class ListBooksComponent implements OnInit {
   public books$: Observable<Book[]>;
   public displayedColumns: string[] = ['name', 'description', 'year'];
   constructor(private bookService: BookService,
@@ -17,12 +17,5 @@ export class ListBooksComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.books$ = this.bookService.getBooks();
-  }
-  ngOnDestroy() {
-    const i = this.router.config.findIndex(x => x.path === 'books');
-    this.router.config.splice(i, 1);
-    this.router.config.push(
-      {path: 'books', loadChildren: () => import('../../../book-handler/book-handler.module').then(mod => mod.BookHandlerModule)}
-    );
   }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,21 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class AuthService {
   public authorized$: Observable<boolean>;
   private authorizedSource: BehaviorSubject<boolean>;
-  constructor() {
+  constructor(private router: Router) {
     this.authorizedSource = new BehaviorSubject<boolean>(false);
     this.authorized$ = this.authorizedSource.asObservable();
   }
+
   public isAuthorized(): boolean {
     return this.authorizedSource.value;
   }
+
   public setAuthorized(value: boolean): void {
     this.authorizedSource.next(value);
+    const i = this.router.config.findIndex(x => x.path === 'books');
+    this.router.config.splice(i, 1);
+    this.router.config.push(
+      {path: 'books', loadChildren: () => import('../../modules/book-handler/book-handler.module').then(mod => mod.BookHandlerModule)}
+    );
   }
 }
