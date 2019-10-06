c631925c6e88e367de54e7408fd264819b5e4afe
gquinteros93 <53574912+gquinteros93@users.noreply.github.com>
2019-10-05 22:00:30 -0300

    improve code

--- src/app/modules/book-manager/book-manager.module.ts
+++ src/app/modules/book-manager/book-manager.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ListBooksComponent } from './components/list-books/list-books.component';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ListBooksComponent],
   imports: [
     CommonModule,
     MatTableModule,
+    MatIconModule,
+    MatButtonModule,
     RouterModule.forChild([{path: '', component: ListBooksComponent}])
   ]
 })
--- src/app/modules/book-manager/components/list-books/list-books.component.ts
+++ src/app/modules/book-manager/components/list-books/list-books.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from 'src/app/shared/models/book';
 import { BookService } from 'src/app/shared/services/book.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-books',
   templateUrl: './list-books.component.html',
   styleUrls: ['./list-books.component.scss']
 })
-export class ListBooksComponent implements OnInit {
-  public books$: Observable<Book[]>;
+export class ListBooksComponent {
   public displayedColumns: string[] = ['name', 'description', 'year'];
-  constructor(private bookService: BookService,
-              private router: Router) { }
-
-  ngOnInit() {
-    this.books$ = this.bookService.getBooks();
+  public get books$(): Observable<Book[]> {
+    return this.bookService.getBooks();
+  }
+  constructor(private bookService: BookService) {
   }
 }
